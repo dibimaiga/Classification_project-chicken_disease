2629b80d55de7fe7f084a3eb86feeffc29633390
dibimaiga <dibimaigapro@gmail.com>
2026-04-22 10:36:36 +0200

    prepare base model completed and some fixes on README and data-ingestion notebook

--- README.md
+++ README.md
@@ -9,5 +9,5 @@
 5. Update the configuration manager in src config
 6. Update the components
 7. Update the pipeline
-8. Update the main.py
+8. Update the main.py <!-- pas besoin quand on experiment ave notebooklm -->
 9. Update the dvc.yaml <!-- I will update this after completed all the components -->
--- main.py
+++ main.py
@@ -2,12 +2,23 @@
 
 from Chicken_disease_classification import logger
 from Chicken_disease_classification.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Chicken_disease_classification.pipeline.Stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare Base Model stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
